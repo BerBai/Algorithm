4b7c0b6ba966c4684576336a19f430be4143c6bb
tooyi <bai5775@outlook.com>
2021-07-19 14:20:47 +0800

    12

---
+++ leetcode/offer/Offer12.java
@@ -0,0 +1,39 @@
+package offer;
+
+/**
+ * @author ber
+ * @version 1.0
+ * @date 21/7/19 08:31
+ */
+public class Offer12 {
+    class solutiona {
+        public boolean exist(char[][] board, String word) {
+            char[] words = word.toCharArray();
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (dfs(board, words, i, j, 0)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public boolean dfs(char[][] board, char[] words, int i, int j, int k) {
+            if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != words[k]) {
+                return false;
+            }
+            if (k == words.length - 1) {
+                return true;
+            }
+            board[i][j] = '\0';
+            boolean res = dfs(board, words, i, j + 1, k + 1)
+                    || dfs(board, words, i - 1, j, k + 1)
+                    || dfs(board, words, i + 1, j, k + 1)
+                    || dfs(board, words, i, j - 1, k + 1);
+            board[i][j] = words[k];
+            return res;
+        }
+    }
+
+}
