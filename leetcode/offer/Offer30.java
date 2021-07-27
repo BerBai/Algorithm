446d60006fbe7a6a68b68d53326ec285f274bbc2
tooyi <bai5775@outlook.com>
2021-07-27 10:37:25 +0800

    30

--- leetcode/offer/Offer30.java
+++ leetcode/offer/Offer30.java
@@ -8,6 +8,7 @@ import java.util.LinkedList;
  * @date 21/7/27 10:10
  */
 public class Offer30 {
+    // LinkedList的add()，是在尾部进。peek()是头部出
     class MinStack {
         LinkedList<Integer> stackA, stackB;
 
@@ -38,6 +39,40 @@ public class Offer30 {
 
         public int min() {
             return stackB.peekLast();
+        }
+    }
+
+    // 区分LinkedList的pop()、push()，元素在头部进、出
+    class MinStack2 {
+        LinkedList<Integer> stackA, stackB;
+
+        /**
+         * initialize your data structure here.
+         */
+        public MinStack2() {
+            stackA = new LinkedList<>();
+            stackB = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            stackA.push(x);
+            if (stackB.isEmpty() || stackB.peek() >= x) {
+                stackB.push(x);
+            }
+        }
+
+        public void pop() {
+            if (stackA.pop().equals(stackB.peek())) {
+                stackB.pop();
+            }
+        }
+
+        public int top() {
+            return stackA.peek();
+        }
+
+        public int min() {
+            return stackB.peek();
         }
     }
 
