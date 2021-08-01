2bf4279293d41e38cc8d8d38860acc529f91ecc2
tooyi <bai5775@outlook.com>
2021-08-01 10:54:26 +0800

    Offer31

---
+++ leetcode/offer/Offer31.java
@@ -0,0 +1,41 @@
+package offer;
+
+import java.util.Stack;
+
+/**
+ * @author ber
+ * @version 1.0
+ * @date 21/8/1 09:01
+ */
+public class Offer31 {
+    class Solution {
+        public boolean validateStackSequences(int[] pushed, int[] popped) {
+            if (pushed == null || pushed.length == 0) {
+                return true;
+            }
+            Stack<Integer> stack = new Stack<>();
+            stack.push(pushed[0]);
+            int i = 1, j = 0, n = pushed.length, m = popped.length;
+            while (!stack.isEmpty()) {
+                if (i == n && j != m && stack.peek() != popped[j]) {
+                    return false;
+                }
+                while (stack.peek() != popped[j] && i < n && pushed[i] != popped[j]) {
+                    stack.push(pushed[i++]);
+                }
+                if (stack.peek() == popped[j] || (i < n && pushed[i] == popped[j])) {
+                    if (stack.peek() == popped[j]) {
+                        stack.pop();
+                    } else {
+                        i++;
+                    }
+                    j++;
+                }
+                if (stack.isEmpty() && i < n) {
+                    stack.push(pushed[i++]);
+                }
+            }
+            return true;
+        }
+    }
+}
