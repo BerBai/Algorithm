dcf209867d61bb9db67723d3dbf5f81f25cba470
tooyi <bai5775@outlook.com>
2021-08-25 19:44:49 +0800

    每日打卡

---
+++ leetcode/daily/T787.java
@@ -0,0 +1,14 @@
+package daily;
+
+/**
+ * @author ber
+ * @version 1.0
+ * @date 21/8/24 08:24
+ */
+public class T787 {
+    class Solution {
+        public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+
+        }
+    }
+}
