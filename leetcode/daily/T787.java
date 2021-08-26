8a78d4009941aa173190632cdcdbc31b3c9e10b2
tooyi <bai5775@outlook.com>
2021-08-26 10:47:26 +0800

    每日打卡

--- leetcode/daily/T787.java
+++ leetcode/daily/T787.java
@@ -1,5 +1,7 @@
 package daily;
 
+import java.util.Arrays;
+
 /**
  * @author ber
  * @version 1.0
@@ -8,7 +10,23 @@ package daily;
 public class T787 {
     class Solution {
         public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-
+            final int INF = 10000 * 101 + 1;
+            int[][] f = new int[k + 2][n];
+            for (int i = 0; i < k + 2; ++i) {
+                Arrays.fill(f[i], INF);
+            }
+            f[0][src] = 0;
+            for (int t = 1; t <= k + 1; ++t) {
+                for (int[] flight : flights) {
+                    int j = flight[0], i = flight[1], cost = flight[2];
+                    f[t][i] = Math.min(f[t][i], f[t - 1][j] + cost);
+                }
+            }
+            int ans = INF;
+            for (int t = 1; t <= k + 1; ++t) {
+                ans = Math.min(ans, f[t][dst]);
+            }
+            return ans == INF ? -1 : ans;
         }
     }
 }
