b8a4ea58507959a8c56ef8aee8f9601f8fbae4de
ber <berbai@qq.com>
2022-02-10 19:18:29 +0800

    最简分数

---
+++ leetcode/daily/T1447.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int simplify(int a, int b) {
+        if (b % a == 0) {
+            return a;
+        }
+        return simplify(b, a % b);
+    }
+
+    public List<String> simplifiedFractions(int n) {
+        if (n == 1) {
+            return new ArrayList<>();
+        }
+        List<String> ans = new ArrayList<>();
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = i+1; j <= n; j++) {
+                if (simplify(i, j) == 1) {
+                    ans.add(i + "/" + j);
+                }
+            }
+        }
+        return ans;
+    }
+}
