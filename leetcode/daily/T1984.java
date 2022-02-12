358413946bca18849ef86c9279c45b58b0824625
ber <bai5775@outlook.com>
2022-02-12 18:32:41 +0800

    滑动窗口

---
+++ leetcode/daily/T1984.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class T1984 {
+    
+}
+
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        if(k==1 || nums.length == 1) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        int ans = nums[nums.length-1];
+        for(int l=0,r=k-1;r<nums.length;) {
+            ans = Math.min(ans, nums[r++]-nums[l++]);
+        }
+        return ans;
+    }
+}
