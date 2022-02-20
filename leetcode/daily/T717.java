923e04371a3a8c9e97cc6e562cadb15d24324bdc
ber <bai5775@outlook.com>
2022-02-20 11:02:21 +0800

    题解

--- leetcode/daily/T717.java
+++ leetcode/daily/T717.java
@@ -4,7 +4,7 @@ public class T717 {
 
 /**
  * 最后一个字符编码必须是一位字符
- * 决定最后一位是否单独成为一个编码，取决于最后的0前面的连续1的个数，如果成偶数，则符合。
+ * 决定最后一位是否单独成为一个编码，取决于最后的0前面的连续1的个数，如果成偶数，则符合；反之则不符合。
  */
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
