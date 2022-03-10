4f4ab56519e63d61ebe6dd0914720345289259ee
tooyi <bai5775@outlook.com>
2022-03-10 15:58:38 +0800

    682

--- leetcode/offer/Offer68_2.java
+++ leetcode/offer/Offer68_2.java
@@ -6,4 +6,25 @@ package offer;
  * @date 22/3/10 15:27
  */
 public class Offer68_2 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null || root.val == p.val || root.val == q.val)
+                return root;
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if (left == null) return right;
+            if (right == null) return left;
+            return root;
+        }
+    }
 }
